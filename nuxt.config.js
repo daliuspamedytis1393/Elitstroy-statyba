14e57cc605870fc04b0666489f776198e84614d0
daliuspamedytis1393 <daliuspamedytis1393@gmail.com>
2023-01-08 12:47:20 +0200

    update website

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,83 +1,83 @@
 export default {
-	// Global page headers: https://go.nuxtjs.dev/config-head
-	mode: 'universal',
-	loading: '~/components/Preloader.vue',
-	head: {
-		title: 'Bonax - Construction & Business Vue Nuxt Js Template',
-		htmlAttrs: {
-			lang: 'en'
-		},
-		meta: [
-			{ charset: 'utf-8' },
-			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
-			{ hid: 'description', name: 'description', content: '' },
-			{ name: 'format-detection', content: 'telephone=no' }
-		],
-		link: [
-			{ rel: 'icon', type: 'image/png', href: '/favicon.png' },
-			{ rel: 'stylesheet', href: "https://fonts.googleapis.com/css2?family=Saira:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200&display=swap" },
-			{ rel: 'stylesheet', href: "https://fonts.googleapis.com/css2?family=Barlow+Condensed:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100&display=swap" }
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  mode: 'universal',
+  loading: '~/components/Preloader.vue',
+  head: {
+    title: 'UAB Elistroy - statybų paslaugos už gerą kainą!',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' },
+    ],
+    link: [
+      { rel: 'icon', type: 'image/png', href: '/favicon.png' },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Saira:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200&display=swap',
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Barlow+Condensed:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100&display=swap',
+      },
+    ],
+  },
 
-		]
-	},
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    '@/assets/css/bootstrap.min.css',
+    '@/assets/css/font-awesome.min.css',
+    '@/assets/css/animate.min.css',
+    '@/assets/css/icofont.min.css',
+    '@/assets/css/aos.css',
+  ],
 
-	// Global CSS: https://go.nuxtjs.dev/config-css
-	css: [
-		'@/assets/css/bootstrap.min.css',
-		'@/assets/css/font-awesome.min.css',
-		'@/assets/css/animate.min.css',
-		'@/assets/css/icofont.min.css',
-		'@/assets/css/aos.css',
-	],
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+    { src: '~/plugins/bootstrap.js', mode: 'client' },
+    { src: '~/plugins/vue-owl-carousel.js', mode: 'client' },
+    '~plugins/vue-fragment',
+    '~plugins/vue-side-menu',
+  ],
 
-	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-	plugins: [
-		{ src: '~/plugins/bootstrap.js', mode: 'client' },
-		{src: '~/plugins/vue-owl-carousel.js', mode: 'client'},
-		'~plugins/vue-fragment',
-		'~plugins/vue-side-menu',
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: {
+    dirs: ['~/components', '~/components/header'],
+  },
 
-	],
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/style-resources',
+  ],
 
-	// Auto import components: https://go.nuxtjs.dev/config-components
-	components: {
-		dirs: [
-			'~/components',
-			'~/components/header'
-		]
-	},
+  styleResources: {
+    scss: [
+      '~/assets/scss/variables.scss',
+      'vue-sidebar-menu/src/scss/vue-sidebar-menu.scss',
+      '~/assets/scss/style.scss',
+    ],
+  },
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+  ],
 
-	// Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-	buildModules: [
-		// https://go.nuxtjs.dev/eslint
-		'@nuxtjs/style-resources'
-	],
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+    baseURL: '/',
+  },
 
-	styleResources: {
-		scss: [
-			'~/assets/scss/variables.scss',
-			'vue-sidebar-menu/src/scss/vue-sidebar-menu.scss',
-			'~/assets/scss/style.scss'
-		]
-	},
-	// Modules: https://go.nuxtjs.dev/config-modules
-	modules: [
-		// https://go.nuxtjs.dev/axios
-		'@nuxtjs/axios'
-	],
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
 
-	// Axios module configuration: https://go.nuxtjs.dev/config-axios
-	axios: {
-		// Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-		baseURL: '/'
-	},
-
-	// Build Configuration: https://go.nuxtjs.dev/config-build
-	build: {},
-
-	// configuration for server to serve form subfolder
-	// router: {
-	// 	base: '/nuxt/'
-	// },
-
+  // configuration for server to serve form subfolder
+  // router: {
+  // 	base: '/nuxt/'
+  // },
 }

e6c3c6bafff97486245f183bacc04009139e1288
daliuspamedytis1393 <daliuspamedytis1393@gmail.com>
2023-01-11 03:36:09 +0200

    update website

--- nuxt.config.js
+++ nuxt.config.js
@@ -67,6 +67,7 @@ export default {
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
+    '@luxdamore/nuxt-prune-html',
     '@nuxtjs/axios',
   ],
 
